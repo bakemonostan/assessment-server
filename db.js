af6f7fb9ff8fd4b014fb9e95690afa6d40a1f458
bakemonostan <ehizomokhomion@gmail.com>
2024-03-26 14:11:55 +0100

    check config

--- db.js
+++ db.js
@@ -1,12 +1,22 @@
-const POOL = require("pg").Pool;
+const { Pool } = require('pg');
+
 require("dotenv").config();
 
-const pool = new POOL({
+let localPoolConfig = {
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
     password: 'edialeomo12',
     port: 5432,
-});
+}
+
+const poolConfig = process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  } : localPoolConfig;
+
+  const pool = new Pool(poolConfig);
 
 module.exports = pool
